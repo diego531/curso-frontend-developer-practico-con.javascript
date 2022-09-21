134b7e0e753bc841a13b69d30ba35622b46a214d
Diego Zapata <dzapata1011@gmail.com>
2022-09-21 18:30:52 -0500

    Cuartos componentes añadidos (segundo aside, configuracion del javascript y estilos

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu)
@@ -13,20 +13,20 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
 
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
        }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
        }
     
     mobileMenu.classList.toggle('inactive');
@@ -40,7 +40,7 @@ function toggleCarritoAside(){
          mobileMenu.classList.add('inactive');
         }
         
-        aside.classList.toggle('inactive');
+        shoppingCartContainer.classList.toggle('inactive');
 
 }
 
